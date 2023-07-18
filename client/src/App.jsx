28748ec6eebe65e5c8d5d2eb0f9c147075b22b05
Bui Nhut Nam <nhutnamtube@gmail.com>
2023-07-18 22:11:54 +0700

    yanr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import { toast } from 'react-hot-toast';
 
 
 axios.defaults.baseURL = 'http://127.0.0.1:4000';
+// axios.defaults.baseURL = 'https://airn-bnb-app-api2.vercel.app';
 axios.defaults.withCredentials = true;
 
 
